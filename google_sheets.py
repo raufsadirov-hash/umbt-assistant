65e4a4abde69985ec75f6b828ff705e1f125ea28
raufsadirov-hash <raufsadirov@gmail.com>
2026-07-15 13:25:06 +0500

    Update google_sheets.py

--- google_sheets.py
+++ google_sheets.py
@@ -3,25 +3,41 @@ import json
 import gspread
 from google.oauth2.service_account import Credentials
 
-scope = [
-    "https://www.googleapis.com/auth/spreadsheets",
-    "https://www.googleapis.com/auth/drive",
-]
 
-creds_dict = json.loads(os.environ["GOOGLE_CREDENTIALS"])
+sheet = None
 
-creds = Credentials.from_service_account_info(
-    creds_dict,
-    scopes=scope
-)
 
-client = gspread.authorize(creds)
+def get_sheet():
+    global sheet
 
-sheet = client.open("UMBT BOT").sheet1
+    if sheet is None:
+        print("Подключаюсь к Google Sheets...")
+
+        scope = [
+            "https://www.googleapis.com/auth/spreadsheets",
+            "https://www.googleapis.com/auth/drive",
+        ]
+
+        creds_dict = json.loads(os.environ["GOOGLE_CREDENTIALS"])
+
+        creds = Credentials.from_service_account_info(
+            creds_dict,
+            scopes=scope
+        )
+
+        client = gspread.authorize(creds)
+
+        sheet = client.open("UMBT BOT").sheet1
+
+        print("Google Sheets подключен!")
+
+    return sheet
 
 
 def save_user(data):
-    sheet.append_row([
+    sh = get_sheet()
+
+    sh.append_row([
         data["fio"],
         data["company"],
         data["position"],
